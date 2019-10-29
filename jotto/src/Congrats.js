0bc6ac18da9019a5191fd2f1f8f6ba826cc3e776
Egor Shum <89135509130@mail.ru>
2019-10-29 08:26:28 +0700

    Next

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+ */
+export default () => {
+  return <div></div>;
+};
