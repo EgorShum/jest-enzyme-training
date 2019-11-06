804d4dcbbbf478b5bb130c022182a3708e3ae1e3
Egor Shum <89135509130@mail.ru>
2019-11-06 07:23:21 +0700

    Unit #67

--- jotto/src/reducers/index.js
+++ jotto/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import success from './successReducer';
+import guessedWordsReducer from './guessedWordsReducer';
 
 export default combineReducers({
   success
